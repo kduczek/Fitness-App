7ae83ae4edca1a75fec752ffe531056144de94f2
Dominik18m <dominik.malesa@unilodz.eu>
2020-03-10 17:32:37 +0100

    Zmodifikowanie klasy Fittnes

--- app/src/main/java/com/example/fitnessapp/Fittnes_centerFragment.java
+++ app/src/main/java/com/example/fitnessapp/Fittnes_centerFragment.java
@@ -10,6 +10,34 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fittnes_centerFragment extends Fragment {
+
+    private String Name;
+    private String Description;
+
+    public Fittnes_centerFragment(String Name,String Description) {
+        this.Name=Name;
+        this.Description=Description;
+    }
+
+    public Fittnes_centerFragment(){
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
