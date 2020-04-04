ae585ee17e417059976775de8062646c3e6f4312
kduczek <duczek.krystian@gmail.com>
2020-04-04 11:05:04 +0200

    Każdy nowy użytkownik przy rejestracji tworzy swoj dokument w kolekcji Users

--- app/src/main/java/com/example/fitnessapp/RegisterActivity.java
+++ app/src/main/java/com/example/fitnessapp/RegisterActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,9 +13,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -22,13 +30,17 @@ public class RegisterActivity extends AppCompatActivity {
     Button btnReg;
     TextView tvReg;
     FirebaseAuth mFirebaseAuth;
+    FirebaseFirestore fStore;
+    String userID;
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseAuth = FirebaseAuth.getInstance(); //otwarcie polaczenia z systemem autoryzacji Firebase
+        fStore = FirebaseFirestore.getInstance(); //otwarcie polaczenia z Firestore
         emailId = findViewById(R.id.editText6);
         password = findViewById(R.id.editText7);
         username = findViewById(R.id.editText5);
@@ -38,10 +50,10 @@ public class RegisterActivity extends AppCompatActivity {
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = emailId.getText().toString();
+                final String email = emailId.getText().toString();
                 String pwd = password.getText().toString();
-                String user = username.getText().toString();
-                if(user.isEmpty())
+                final String usernm = username.getText().toString();
+                if(usernm.isEmpty())
                 {
                     username.setError("Podaj nazwę użytkownika");
                     username.requestFocus();
@@ -56,12 +68,17 @@ public class RegisterActivity extends AppCompatActivity {
                     password.setError("Podaj hasło");
                     password.requestFocus();
                 }
+                else if(pwd.length() < 6)
+                {
+                    password.setError("Hasło musi mieć conajmniej 6 znaków");
+                    password.requestFocus();
+                }
                 else if(email.isEmpty() && pwd.isEmpty())
                 {
                     Toast.makeText(RegisterActivity.this, "Uzupełnij oba pola!", Toast.LENGTH_SHORT).show();
                 }
                 else if(!(email.isEmpty() && pwd.isEmpty()))
-                {
+                {   //rejestracja uzytkownika
                     mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
@@ -71,6 +88,17 @@ public class RegisterActivity extends AppCompatActivity {
                             }
                             else
                             {
+                                userID = mFirebaseAuth.getCurrentUser().getUid();
+                                DocumentReference documentReference = fStore.collection("users").document(userID);
+                                Map<String, Object> user = new HashMap<>();
+                                user.put("username", usernm);
+                                user.put("email", email);
+                                documentReference.set(user).addOnFailureListener(new OnFailureListener() {
+                                    @Override //log w razie niepowodzenia
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.d(TAG, "onFailure " + e.toString());
+                                    }
+                                });
                                 startActivity(new Intent(RegisterActivity.this, MainActivity.class));
                             }
                         }
