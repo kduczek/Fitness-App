882f416f462545fd72b1114fb9e8e8330726295a
kduczek <duczek.krystian@gmail.com>
2020-04-14 14:14:35 +0200

    Zmiany kosmetyczne ekranów logowania i rejestracji oraz toolbara. Usunięcie zakomentowanego kodu, którego zapomniałem usunąć poprzednim razem

--- app/src/main/java/com/example/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/fitnessapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fitnessapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,11 +12,18 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
@@ -23,6 +31,7 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
     Toolbar toolbar;
     NavigationView navigationView;
     FirebaseAuth mFirebaseAuth;
+    TextView user;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
@@ -47,40 +56,11 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
 
         getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment, new MainFragment()).commit();
 
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        drawer = findViewById(R.id.drawer_layout);
-//        NavigationView navigationView = findViewById(R.id.nav_view);
-//        navigationView.setNavigationItemSelectedListener(this);
-//
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.addDrawerListener(toggle);
-//        toggle.syncState();
     }
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-//        switch (item.getItemId()) {
-//            case R.id.nav_settings: //here we can add more button actions from left drawer menu
-//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
-//                break;
-//            case R.id.nav_plany: //here we can add more button actions from left drawer menu
-//                Intent intent = new Intent(this,Plany.class);
-//                startActivity(intent);
-//                break;
-//            case R.id.nav_kreator: //here we can add more button actions from left drawer menu
-//                Intent intent2 = new Intent(this,Kreator.class);
-//                startActivity(intent2);
-//                break;
-//            case R.id.nav_logout:
-//                FirebaseAuth.getInstance().signOut();
-//                Toast.makeText(this, "Wylogowano", Toast.LENGTH_SHORT).show();
-//                Intent intToLogin = new Intent(MainActivity.this, LoginActivity.class);
-//                startActivity(intToLogin);
-//                break;
-//        }
-//        drawer.closeDrawer(GravityCompat.START);
         switch(menuItem.getItemId())
         {
             case R.id.nav_home:
--- app/src/main/java/com/example/fitnessapp/SettingsFragment.java
+++ app/src/main/java/com/example/fitnessapp/SettingsFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class SettingsFragment extends Fragment {
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
